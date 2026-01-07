fcdbeb3b3ac1539b9817540c5a59767e78dd2d73
ginahale10 <ginahale10@outlook.com>
2026-01-07 14:43:02 +0700

    basic skeleton code for controller and service completed

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UUID } from 'crypto';
 
 @Injectable()
 export class AppService {
+
+  fetchUser(id: string) {
+    return {
+      id: id,
+      name: '',
+      email: 'jwbevbwiv',
+      status: 'ACCEPTED',
+      role: 'POO',
+      created: '06/01/26',
+      updated: '07/01/26'
+    }
+  }
+
+  fetchUsers() {
+    return [
+      {
+
+      },
+      {
+
+      }
+    ]
+  }
+
+
+
   getHello(): string {
     return 'Hello Annie!';
   }
